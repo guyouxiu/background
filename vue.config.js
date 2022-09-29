39d185489417205611bfbf89078740e8b0720ec2
guyouxiu <1063860931@qq.com>
2022-09-29 09:35:31 +0800

    对项目进行基本配置，修改浏览器名称

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8889,
+    host: "localhost",
+    open: true,
+    https: false
+  },
+  lintOnSave:false,
+  productionSourceMap:false
 })
