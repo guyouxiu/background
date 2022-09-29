7975aedded7e1366d4539f504b3e3aa0424fef16
guyouxiu <1063860931@qq.com>
2022-09-29 09:53:48 +0800

    配置跨域

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,19 @@ module.exports = defineConfig({
     port: 8889,
     host: "localhost",
     open: true,
-    https: false
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_PASE_API]: {
+        target: 'VUE_APP_SERVE_URL',
+        changeOrigin: true,
+        pathRewrite: {
+          "^/guyouxiu": ''
+        }
+
+
+      }
+    }
   },
-  lintOnSave:false,
-  productionSourceMap:false
+  lintOnSave: false,
+  productionSourceMap: false
 })
